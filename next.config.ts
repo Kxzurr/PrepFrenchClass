9fbeab983bbaca91030680672fac9a2eef092150
Kxzurr <blizzard22011@gmail.com>
2026-02-21 08:50:02 +0530

    g

--- next.config.ts
+++ next.config.ts
@@ -12,34 +12,34 @@ const nextConfig: NextConfig = {
     ],
   },
   async rewrites() {
-    return [
-      {
-        source: "/blog",
-        destination:
-          "https://darkseagreen-turtle-359545.hostingersite.com/blog",
-      },
-      {
-        source: "/blog/:path*",
-        destination:
-          "https://darkseagreen-turtle-359545.hostingersite.com/blog/:path*",
-      },
-      {
-        source: "/wp-admin/:path*",
-        destination:
-          "https://darkseagreen-turtle-359545.hostingersite.com/wp-admin/:path*",
-      },
-      {
-        source: "/wp-login.php",
-        destination:
-          "https://darkseagreen-turtle-359545.hostingersite.com/wp-login.php",
-      },
-      {
-        source: "/wp-json/:path*",
-        destination:
-          "https://darkseagreen-turtle-359545.hostingersite.com/wp-json/:path*",
-      },
-    ];
-  }
+  return [
+    {
+      source: "/blog",
+      destination:
+        "https://darkseagreen-turtle-359545.hostingersite.com/blog",
+    },
+    {
+      source: "/blog/:path*",
+      destination:
+        "https://darkseagreen-turtle-359545.hostingersite.com/blog/:path*",
+    },
+    {
+      source: "/wp-admin/:path*",
+      destination:
+        "https://darkseagreen-turtle-359545.hostingersite.com/wp-admin/:path*",
+    },
+    {
+      source: "/wp-login.php",
+      destination:
+        "https://darkseagreen-turtle-359545.hostingersite.com/wp-login.php",
+    },
+    {
+      source: "/wp-json/:path*",
+      destination:
+        "https://darkseagreen-turtle-359545.hostingersite.com/wp-json/:path*",
+    },
+  ];
+}
 };
 
 export default nextConfig;
