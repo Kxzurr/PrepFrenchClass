b185646f7d2473ea3fcd850518febe6decb021eb
Kxzurr <blizzard22011@gmail.com>
2026-02-21 09:16:13 +0530

    blog

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,12 @@ const nextConfig: NextConfig = {
     {
       source: "/blog",
       destination:
-        "https://darkseagreen-turtle-359545.hostingersite.com/blog",
+        "https://darkseagreen-turtle-359545.hostingersite.com",
     },
     {
       source: "/blog/:path*",
       destination:
-        "https://darkseagreen-turtle-359545.hostingersite.com/blog/:path*",
+        "https://darkseagreen-turtle-359545.hostingersite.com/:path*",
     },
     {
       source: "/wp-admin/:path*",
