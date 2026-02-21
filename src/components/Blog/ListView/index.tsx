4868bc123c8624456911bc4a1b1b3049e5634010
Kxzurr <blizzard22011@gmail.com>
2026-02-21 12:11:43 +0530

    a

--- src/components/Blog/ListView/index.tsx
+++ src/components/Blog/ListView/index.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import BlogListSection from './BlogListSection';
 
 export default function BlogListView() {
     return (
         <>
-            <BlogListSection />
+            <Suspense fallback={<div className="py-20 text-center">Loading blog posts...</div>}>
+                <BlogListSection />
+            </Suspense>
         </>
     );
 }
